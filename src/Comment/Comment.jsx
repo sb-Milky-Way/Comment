c71340a6e517a22756f50ff68b0f4ff7a616a6e5
mikolaj <mikolaj.burawski@gmail.com>
2017-04-14 14:33:46 +0100

    Refactor to BEM

--- src/Comment/Comment.jsx
+++ src/Comment/Comment.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
 import cn from 'classnames';
 import importcss from 'importcss';
 import styles from './Comment.css';
@@ -8,36 +8,36 @@ import FavoriteIcon from 'react-icons/lib/md/favorite-outline';
 @importcss(styles)
 class Comment extends Component {
   static propTypes = {
-    text : PropTypes.string.isRequired,
-    author : PropTypes.string.isRequired,
-    time : PropTypes.instanceOf(Date),
-    customAvatar : PropTypes.node
-  }
+    text: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    time: PropTypes.instanceOf(Date),
+    customAvatar: PropTypes.node
+  };
 
   static defaultProps = {
-    time : new Date()
-  }
+    time: new Date()
+  };
 
   render() {
-    const { author, text, time, customAvatar } = this.props;
+    const {author, text, time, customAvatar} = this.props;
 
-    const avatar = customAvatar || <div styleName="defaultAvatar">{author.split('').shift().toUpperCase()}</div>;
+    const avatar = customAvatar || <div styleName="comment__avatar">{author.split('').shift().toUpperCase()}</div>;
 
     return (
-      <div styleName="commentContainer">
-        <div styleName="userAvatarContainer">
-            {avatar}
+      <section styleName="comment">
+        <div styleName="comment__avatar-container">
+          {avatar}
         </div>
-        <div styleName="commentContent">
-            <div styleName="commentActions"><ClearIcon /></div>
-            <div styleName="commentAuthor">{author}</div>
-            <div styleName="commentText">{text}</div>
-            <div styleName="commentFooter">
-              {time.toISOString().slice(0, 10)}
-              <div styleName="commentFooterActions"><FavoriteIcon /></div>
-            </div>
+        <div styleName="comment__content">
+          <div styleName="comment__actions"><ClearIcon /></div>
+          <div styleName="comment__author">{author}</div>
+          <div styleName="comment__text">{text}</div>
+          <footer styleName="comment__footer">
+            <div styleName="comment__footer-actions"><FavoriteIcon /></div>
+            <div className="comment__meta">{time.toISOString().slice(0, 10)}</div>
+          </footer>
         </div>
-      </div>
+      </section>
     );
   }
 }
